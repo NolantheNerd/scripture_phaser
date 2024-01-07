d1db73f49af94e4af3ec3857835186dd5eb5195e
Nolan <nolanmcmahonemail@gmail.com>
2024-01-06 17:17:39 -0700

    Response to #4

--- src/api.py
+++ src/api.py
@@ -216,14 +216,9 @@ class API:
         if not self.filename.exists():
             text = ""
         else:
-            if platform.system() == "Windows":
-                with open(withextension, "r") as file:
-                    text = file.readlines()
-                    text = "".join(text)
-            else:
-                with open(self.filename, "r") as file:
-                    text = file.readlines()
-                    text = "".join(text)
+            with open(self.filename, "r") as file:
+                text = file.readlines()
+                text = "".join(text)
 
             # Vim Automatically Adds a Newline at the End of the File when
             # you save it. (Unless you set :nofixeol and set :nofixendofline -
