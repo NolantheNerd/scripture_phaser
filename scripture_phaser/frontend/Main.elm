bdd0b6acb66c43914b09669c221b1b231b85ae10
Nolan <nolanmcmahonemail@gmail.com>
2024-09-26 13:24:26 -0600

    WIP on Web Frontend in Elm

---
+++ scripture_phaser/frontend/Main.elm
@@ -0,0 +1,91 @@
+{-
+   scripture_phaser helps you to memorize the Bible.
+   Copyright (C) 2023-2024 Nolan McMahon
+   
+   This file is part of scripture_phaser.
+   
+   scripture_phaser is licensed under the terms of the BSD 3-Clause License
+   
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+   
+   1. Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+   
+   2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+   
+   3. Neither the name of the copyright holder nor the names of its contributors
+   may be used to endorse or promote products derived from this software without
+   specific prior written permission.
+   
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+   POSSIBILITY OF SUCH DAMAGE.
+-}
+module Main exposing (..)
+
+import Browser
+import Reference exposing (Reference, getText)
+import Html exposing (Html, select, option, div, text)
+
+-- Main
+
+main = Browser.sandbox { init = init, update = update, view = view }
+
+-- Model
+
+allTranslations = [ "KJV", "WEB", "ESV", "NIV", "NKJV", "NLT", "NASB", "NRSV" ]
+type alias Model = {
+  translation : String,
+  one_verse_recitation : Bool,
+  complete_recitation : Bool,
+  include_verse_numbers : Bool,
+  fast_recitations : Bool,
+  reference : List Reference
+  }
+
+init : Model
+init = {
+  translation = "DERP-DEE-DERP",
+  one_verse_recitation = False,
+  complete_recitation = False,
+  include_verse_numbers = False,
+  fast_recitations = False,
+  reference = ""
+  }
+
+-- Update
+
+type Msg = Change String
+
+update : Msg -> Model -> Model
+update msg model = 
+  case msg of
+    Change newTranslation ->
+      { model | translation = newTranslation }
+
+-- View
+
+removeSelectedFromList : List String -> String -> List String
+removeSelectedFromList list selected =
+  List.filter (\l -> l /= selected) list
+
+listToSelect : List String -> List ( Html msg )
+listToSelect list =
+  List.map (\l -> option [] [ text l ] ) list
+
+view : Model -> Html Msg
+view model =
+  select [] ( option [] [ text model.translation ] ::
+    listToSelect ( removeSelectedFromList allTranslations model.translation )
+    )
